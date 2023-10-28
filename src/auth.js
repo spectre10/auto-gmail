00175a3c8334fb2f61efdcebdf0d89db8888eac4
spectre10 <shyamthakkar001@gmail.com>
2023-10-28 19:26:23 +0530

    add -reset flag to change accounts or permissions.

--- src/auth.js
+++ src/auth.js
@@ -2,7 +2,8 @@ import path from 'path'
 import process from 'process'
 import { authenticate } from '@google-cloud/local-auth';
 import { google } from 'googleapis';
-import fs from 'fs/promises'
+import fs from 'fs'
+
 
 
 // If modifying these scopes, delete token.json.
@@ -11,15 +12,15 @@ const SCOPES = ['https://www.googleapis.com/auth/gmail.modify', 'https://www.goo
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+const TOKEN_PATH = path.join(process.cwd(), 'src/token.json');
 
 // The file credentials.json is credentials file downloaded from Google Cloud Console.
-const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'src/credentials.json');
 
 // check if token.json already exist.
 async function loadSavedCredentialsIfExist() {
   try {
-    const content = await fs.readFile(TOKEN_PATH);
+    const content = await fs.promises.readFile(TOKEN_PATH);
     const credentials = JSON.parse(content);
     return google.auth.fromJSON(credentials);
   } catch (err) {
@@ -29,7 +30,7 @@ async function loadSavedCredentialsIfExist() {
 
 // save the payload to token.json file
 async function saveCredentials(client) {
-  const content = await fs.readFile(CREDENTIALS_PATH);
+  const content = await fs.promises.readFile(CREDENTIALS_PATH);
   const keys = JSON.parse(content);
   const key = keys.installed || keys.web;
   const payload = JSON.stringify({
@@ -38,10 +39,17 @@ async function saveCredentials(client) {
     client_secret: key.client_secret,
     refresh_token: client.credentials.refresh_token,
   });
-  await fs.writeFile(TOKEN_PATH, payload);
+  await fs.promises.writeFile(TOKEN_PATH, payload);
 }
 
 export async function authorize() {
+  //if we want to continue with new account or new permissions.
+  if (process.argv[2] === '-reset') {
+    if (fs.existsSync("./src/token.json")) {
+      await fs.promises.rm("./src/token.json")
+    }
+  }
+  
   let client = await loadSavedCredentialsIfExist();
   if (client) {
     return client;
@@ -52,7 +60,7 @@ export async function authorize() {
     scopes: SCOPES,
     keyfilePath: CREDENTIALS_PATH,
   });
-  
+
   if (client.credentials) {
     await saveCredentials(client);
   }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import { google } from 'googleapis';
 import { queryThreads } from './helper.js'
 import { checkExistingLabels } from './handleLabels.js';
 import { authorize } from './auth.js';
+import fs from 'fs/promises';
+
+
 
 // initiallizes the gmail client and start to poll for emails every 120 seconds.
 async function startPolling(auth) {
-  
+
   // initiallize gmail client.
   const gmail = google.gmail({ version: 'v1', auth });
 
@@ -18,7 +21,7 @@ async function startPolling(auth) {
 
   //check if AUTO_REPLIED label already exists.
   checkExistingLabels(labels)
-  
+
   // poll every 120 seconds.
   setInterval(() => {
     queryThreads(gmail)
